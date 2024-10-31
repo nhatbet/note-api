7895431a9eebc9c388c92478a8d04fcbd535f141
nhathdtk <nhat0x9@gmail.com>
2024-10-31 21:08:22 +0700

    fix: migrate table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserTableSeeder;
+use Database\Seeders\CategoryTableSeeder;
+use Database\Seeders\TagTableSeeder;
+use Database\Seeders\ArticleTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(TagTableSeeder::class);
+        $this->call(ArticleTableSeeder::class);
     }
 }
